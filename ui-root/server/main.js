edf19bcdba65d68934d9b4080159481349e5604c
0sm0s1z <mtoussain@gmail.com>
2018-03-29 12:07:46 -0500

    Base Meteor Build

---
+++ ui-root/server/main.js
@@ -0,0 +1,8 @@
+import { Meteor } from 'meteor/meteor';
+
+import '../imports/api/tasks.js';
+import '../imports/api/nessus_ingest.js';
+
+Meteor.startup(() => {
+  // code to run on server at startup
+});
