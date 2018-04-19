a07178eed746c11735b4cfe86b7bdc38c3c116f7
Mike Winstead <mike@winstead.us>
2018-04-19 09:36:10 -0500

    Store uploaded images into GridFS
    * Add JetBrains IDE directory to project-level gitignore
    * Add local meteor data to meteor-level gitignore
    * Add gridfs and babel dependencies to package requirements
    * Change collection in ```api/images``` to be backed by GridFS

--- ui-root/imports/api/images.js
+++ ui-root/imports/api/images.js
@@ -1,15 +1,79 @@
+/**
+ * All functionality regarding handling & storage of images
+ */
 import { Meteor } from 'meteor/meteor';
+import { MongoInternals } from 'meteor/mongo';
+
 import { FilesCollection } from 'meteor/ostrio:files';
 
-export const Images = new FilesCollection({
-  collectionName: 'Images',
-  allowClientCode: false, // Disallow remove files from Client
-  storagePath: '/Users/0zm0z1z/',
-  onBeforeUpload(file) {
-    // Allow upload files under 10MB, and only in png/jpg/jpeg formats
-    if (file.size <= 10485760 && /png|jpg|jpeg/i.test(file.extension)) {
+import Grid from 'gridfs-stream';
+import fs from 'fs';
+
+let gfs;
+if (Meteor.isServer) {
+    gfs = Grid(
+        MongoInternals.defaultRemoteCollectionDriver().mongo.db,
+        MongoInternals.NpmModule
+    );
+}
+
+function handleImageBeforeUpload(file) {
+    if (file.size <= 10485760 && /png|jpg|jpeg/i.test(file.extension)){
       return true;
     }
     return 'Please upload image, with size equal or less than 10MB';
-  }
+}
+
+function handleImageAfterUpload(image) {
+    // Move file to GridFS
+    Object.keys(image.versions).forEach(versionName => {
+        const metadata = { versionName, imageId: image._id, storedAt: new Date() }; // Optional
+        const writeStream = gfs.createWriteStream({ filename: image.name, metadata });
+
+        fs.createReadStream(image.versions[versionName].path).pipe(writeStream);
+
+        writeStream.on('close', Meteor.bindEnvironment(file => {
+            const property = `versions.${versionName}.meta.gridFsFileId`;
+
+            // If we store the ObjectID itself, Meteor (EJSON?) seems to convert it to a
+            // LocalCollection.ObjectID, which GFS doesn't understand.
+            this.collection.update(image._id, { $set: { [property]: file._id.toString() } });
+            this.unlink(this.collection.findOne(image._id), versionName); // Unlink files from FS
+        }));
+    });
+}
+
+function handleImageInterceptDownload(http, image, versionName) {
+    // Serve file from GridFS
+    const _id = (image.versions[versionName].meta || {}).gridFsFileId;
+    if (_id) {
+        const readStream = gfs.createReadStream({ _id });
+        readStream.on('error', err => { throw err; });
+        readStream.pipe(http.response);
+    }
+    return Boolean(_id); // Serve file from either GridFS or FS if it wasn't uploaded yet
+}
+
+function handleImageOnAfterRemove(images) {
+    // Remove corresponding file from GridFS
+    images.forEach(image => {
+        Object.keys(image.versions).forEach(versionName => {
+            const _id = (image.versions[versionName].meta || {}).gridFsFileId;
+            if (_id) gfs.remove({ _id }, err => { if (err) throw err; });
+        });
+    });
+}
+
+export const Images = new FilesCollection({
+    collectionName: "Images",
+    allowClientCode: false,
+    debug: Meteor.isServer && process.env.NODE_ENV === 'development',
+    onBeforeUpload:     handleImageBeforeUpload,
+    onAfterUpload:      handleImageAfterUpload,
+    interceptDownload:  handleImageInterceptDownload,
+    onAfterRemove:      handleImageOnAfterRemove,
 });
+
+if (Meteor.isServer) {
+    Images.denyClient()
+}
